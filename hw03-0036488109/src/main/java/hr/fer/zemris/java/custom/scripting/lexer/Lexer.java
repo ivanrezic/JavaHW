33b077bc1c69cecd1da70b85b89617347921062c
ivan.rezic <ivan.rezic1@gmail.com>
2017-03-20 02:33:06 +0100

    lexer veliki regex napisan

--- hw03-0036488109/src/main/java/hr/fer/zemris/java/custom/scripting/lexer/Lexer.java
+++ hw03-0036488109/src/main/java/hr/fer/zemris/java/custom/scripting/lexer/Lexer.java
@@ -1,23 +1,51 @@
 package hr.fer.zemris.java.custom.scripting.lexer;
 
+import hr.fer.zemris.java.custom.collections.ArrayIndexedCollection;
+
 public class Lexer {
-	private LexerState state;
 	private String[] document;
-	private Token token;
+	private ArrayIndexedCollection helperCollector;
+	private Token[] tokens;
 	private int currentIndex;
 
 	public Lexer(String text) {
 		if (text == null) {
 			throw new IllegalArgumentException("Input text shouldnt be null!");
 		}
-		this.state = LexerState.TEXT;
 		this.document = text.split("(?=(\\{\\$))|(?<=(\\$\\}))");
+		helperCollector= new ArrayIndexedCollection();
+		tokenizer();
 	}
 
-	public Token getToken() {
-		return token;
+	private void tokenizer() {
+		for (String part : document) {
+			if (part.startsWith("{")) {
+				processTag(part);
+			} else {
+				processText(part);
+			}
+		}
 	}
-	
-	
-	
+
+	public void processText(String part) {
+		char[] charParts = part.toCharArray();
+		String accuratePart;
+
+		for (int i = 0; i < charParts.length; i++) {
+			if (charParts[i] == '\\' && (charParts[i + 1] != '{' || charParts[i + 1] != '\\')) {
+				throw new LexerException("After \\ there should be no characters except { and \\.");
+			}
+		}
+
+		accuratePart = new String(charParts).replace("\\", "\\\\").replace("{", "\\{");
+		helperCollector.add(new Token(TokenType.TEXT, accuratePart));
+	}
+
+	public void processTag(String part) {
+		part.replace("{$", "").replaceAll("\\s+", "");
+		helperCollector.add(new Token(TokenType.TAG, "{$"));
+		
+		
+	}
+
 }
