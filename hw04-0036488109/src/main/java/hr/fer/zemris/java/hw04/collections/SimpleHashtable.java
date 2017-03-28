8e23285470e4fca38c0046b11ed584c1b8b70170
ivan.rezic <ivan.rezic1@gmail.com>
2017-03-28 03:19:52 +0200

    treba popraviti iterator

--- hw04-0036488109/src/main/java/hr/fer/zemris/java/hw04/collections/SimpleHashtable.java
+++ hw04-0036488109/src/main/java/hr/fer/zemris/java/hw04/collections/SimpleHashtable.java
@@ -1,28 +1,31 @@
 package hr.fer.zemris.java.hw04.collections;
 
-import static java.lang.Math.ceil;
-import static java.lang.Math.pow;
 import static java.lang.Math.abs;
-
-import java.util.Arrays;
-
+import static java.lang.Math.ceil;
 import static java.lang.Math.log10;
+import static java.lang.Math.pow;
 
-public class SimpleHashtable<K, V> {
+import java.util.Iterator;
+
+public class SimpleHashtable<K, V> implements Iterable<SimpleHashtable.TableEntry<K, V>> {
 	private final static int DEFAULT_CAPACITY = 16;
+	private final static int RESIZE_FACTOR = 2;
 	private int size;
+	private int slotLimit;
 	private TableEntry<K, V>[] table;
 
 	public SimpleHashtable() {
 		table = new TableEntry[DEFAULT_CAPACITY];
+		slotLimit = (int) (0.75 * DEFAULT_CAPACITY);
 	}
 
 	public SimpleHashtable(int capacity) {
 		if (capacity < 1) {
 			throw new IllegalArgumentException("Capacity can not be less than 1.");
 		} else {
-			double temporary = ceil(log10(capacity) / log10(2));
-			table = new TableEntry[(int) pow(capacity, temporary)];
+			int temporary = (int) pow(2, ceil(log10(capacity) / log10(2)));
+			slotLimit = (int) (0.75 * temporary);
+			table = new TableEntry[temporary];
 		}
 	}
 
@@ -96,10 +99,50 @@ public class SimpleHashtable<K, V> {
 		if (key == null) {
 			throw new IllegalArgumentException("Key can not be null.");
 		} else if (!containsKey(key)) {
+			slotLimitReached();
 			addTableEntry(key, value);
 		} else {
 			updateTableEntry(key, value);
 		}
+	}
+
+	private void slotLimitReached() {
+		int count = 0;
+
+		for (int i = 0; i < table.length; i++) {
+			if (table[i] != null) {
+				count++;
+			}
+		}
+
+		if (count >= slotLimit) {
+			resizeTable();
+			slotLimit = (int) (0.75 * table.length);
+		}
+	}
+
+	private void resizeTable() {
+		TableEntry<K, V>[] oldTable = table;
+		table = new TableEntry[oldTable.length * RESIZE_FACTOR];
+		size = 0;
+		for (int i = 0; i < oldTable.length; i++) {
+			TableEntry<K, V> tracker = oldTable[i];
+			while (tracker != null) {
+				put(tracker.key, tracker.value);
+				tracker = tracker.next;
+			}
+		}
+	}
+
+	private void updateTableEntry(K key, V value) {
+		int slot = abs(key.hashCode()) % table.length;
+		TableEntry<K, V> temporary = table[slot];
+
+		while (!temporary.getKey().equals(key)) {
+			temporary = temporary.next;
+		}
+
+		temporary.setValue(value);
 	}
 
 	private void addTableEntry(K key, V value) {
@@ -111,7 +154,7 @@ public class SimpleHashtable<K, V> {
 		} else {
 			addIntoSlot(slot, newEntry);
 		}
-		
+
 		size++;
 	}
 
@@ -125,25 +168,14 @@ public class SimpleHashtable<K, V> {
 		temporary.next = newEntry;
 	}
 
-	private void updateTableEntry(K key, V value) {
-		int slot = abs(key.hashCode()) % table.length;
-		TableEntry<K, V> temporary = table[slot];
-
-		while (temporary.getKey() != key) {
-			temporary = temporary.next;
-		}
-
-		temporary.setValue(value);
-	}
-
 	public V get(Object key) {
 		if (key == null) {
 			return null;
 		}
-		
+
 		int slot = abs(key.hashCode()) % table.length;
 		TableEntry<K, V> temporary = table[slot];
-		
+
 		while (temporary != null) {
 			if (temporary.getKey() == key) {
 				return temporary.getValue();
@@ -159,13 +191,21 @@ public class SimpleHashtable<K, V> {
 	}
 
 	public boolean containsKey(Object key) {
-		return get(key) != null;
+		for (int i = 0, size = table.length; i < size; i++) {
+			for (TableEntry<K, V> tracker = table[i]; tracker != null; tracker = tracker.next) {
+				if (tracker.getKey().equals(key)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 
 	public boolean containsValue(Object value) {
 		for (int i = 0, size = table.length; i < size; i++) {
 			for (TableEntry<K, V> tracker = table[i]; tracker != null; tracker = tracker.next) {
-				if (tracker.getValue().equals(value)) {
+				if (tracker.value == null || (tracker.value != null && tracker.value.equals(value))) {
 					return true;
 				}
 			}
@@ -182,16 +222,28 @@ public class SimpleHashtable<K, V> {
 		int slot = abs(key.hashCode()) % table.length;
 		TableEntry<K, V> temporary = table[slot];
 
-		while (temporary.next.getKey() != key) {
+		while (temporary.next != null && temporary.next.getKey() != key) {
 			temporary = temporary.next;
 		}
 
-		temporary.next = temporary.next.next;
+		if (temporary.next == null) {
+			temporary.next = null;
+		} else {
+			temporary.next = temporary.next.next;
+		}
 		size--;
 	}
 
 	public boolean isEmpty() {
 		return size == 0;
+	}
+
+	public void clear() {
+		for (int i = 0; i < table.length; i++) {
+			table[i] = null;
+		}
+
+		size = 0;
 	}
 
 	@Override
@@ -209,5 +261,48 @@ public class SimpleHashtable<K, V> {
 		}
 
 		return sBuilder.append("]").toString();
+	}
+
+	@Override
+	public Iterator<TableEntry<K, V>> iterator() {
+		return new IteratorImpl();
+
+	}
+
+	private class IteratorImpl implements Iterator<SimpleHashtable.TableEntry<K, V>> {
+		private int slotPosition = 0;
+		private TableEntry<K, V> helper = table[slotPosition];
+
+		public boolean hasNext() {
+			for (int i = slotPosition; i < table.length; i++) {
+				if (helper == null) {
+					if (slotPosition < table.length - 1) {
+						helper = table[++slotPosition];
+					}
+					continue;
+				}
+				while (helper != null) {
+					if (helper.next == null) {
+						if (slotPosition < table.length - 1) {
+							helper = table[++slotPosition];
+						}
+						break;
+					}
+					helper = helper.next;
+					return true;
+				}
+				if (slotPosition < table.length - 1) {
+					helper = table[++slotPosition];
+				}
+			}
+			return false;
+		}
+
+		public SimpleHashtable.TableEntry next() {
+			return helper;
+		}
+
+		public void remove() {
+		}
 	}
 }
---
+++ hw04-0036488109/src/test/java/hr/fer/zemris/java/hw04/collections/SimpleHashtableTest.java
@@ -0,0 +1,14 @@
+package hr.fer.zemris.java.hw04.collections;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleHashtableTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
