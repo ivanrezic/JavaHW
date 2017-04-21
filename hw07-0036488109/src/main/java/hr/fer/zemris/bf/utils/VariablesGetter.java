fd3ef35581a06f9e0c78a09e43a244467d3216f9
ivan.rezic <ivan.rezic1@gmail.com>
2017-04-21 21:33:06 +0200

    treci gotov

---
+++ hw07-0036488109/src/main/java/hr/fer/zemris/bf/utils/VariablesGetter.java
@@ -0,0 +1,64 @@
+package hr.fer.zemris.bf.utils;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import hr.fer.zemris.bf.model.BinaryOperatorNode;
+import hr.fer.zemris.bf.model.ConstantNode;
+import hr.fer.zemris.bf.model.Node;
+import hr.fer.zemris.bf.model.NodeVisitor;
+import hr.fer.zemris.bf.model.UnaryOperatorNode;
+import hr.fer.zemris.bf.model.VariableNode;
+
+public class VariablesGetter implements NodeVisitor {
+
+	private Set<String> variables = new TreeSet<>();
+
+	public Set<String> getVariables() {
+		return variables;
+	}
+
+	@Override
+	public void visit(ConstantNode node) {
+	}
+
+	@Override
+	public void visit(VariableNode node) {
+		variables.add(node.getName());
+	}
+
+	@Override
+	public void visit(UnaryOperatorNode node) {
+		unaryVariables(node);
+	}
+
+	@Override
+	public void visit(BinaryOperatorNode node) {
+		binaryVariables(node);
+	}
+
+	private void unaryVariables(UnaryOperatorNode node) {
+		Node child = node.getChild();
+
+		if (child instanceof VariableNode) {
+			child.accept(this);
+		} else if (child instanceof BinaryOperatorNode) {
+			binaryVariables((BinaryOperatorNode) child);
+		} else {
+			unaryVariables((UnaryOperatorNode) child);
+		}
+	}
+
+	private void binaryVariables(BinaryOperatorNode node) {
+		for (Node n : node.getChildren()) {
+			if (n instanceof VariableNode) {
+				n.accept(this);
+			} else if (n instanceof BinaryOperatorNode) {
+				binaryVariables((BinaryOperatorNode) n);
+			} else {
+				unaryVariables((UnaryOperatorNode) n);
+			}
+		}
+	}
+
+}
