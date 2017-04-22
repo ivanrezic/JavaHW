c74ef75fae47d380414879c5978a337cb9245e7f
ivan.rezic <ivan.rezic1@gmail.com>
2017-04-22 16:04:34 +0200

    finished

--- hw07-0036488109/src/main/java/hr/fer/zemris/bf/parser/Parser.java
+++ hw07-0036488109/src/main/java/hr/fer/zemris/bf/parser/Parser.java
@@ -12,14 +12,33 @@ import hr.fer.zemris.bf.model.Node;
 import hr.fer.zemris.bf.model.UnaryOperatorNode;
 import hr.fer.zemris.bf.model.VariableNode;
 
+/**
+ * <code>Parser</code> represents object which processes text through syntax
+ * analysis. After processing text is divided in syntax groups which are later
+ * encapsulated in nodes used for construction of syntax tree. There are trees
+ * main groups, variables, constants and operators.Parser uses recursive descent
+ * to create tree.
+ *
+ * @author Ivan Rezic
+ */
 public class Parser {
 
+	/** Lexer used in this parser. */
 	private Lexer lexer;
+
+	/** Root node of our tree. */
 	private Node node;
 
+	/**
+	 * Constructor which instantiates new parser. And call first method of our
+	 * recursive descent.
+	 *
+	 * @param expression
+	 *            The epression.
+	 */
 	public Parser(String expression) {
 		if (expression == null) {
-			throw new ParserException("Expression mustn't be null");
+			throw new ParserException("Expression can not be null.");
 		}
 
 		this.lexer = new Lexer(expression);
@@ -32,10 +51,50 @@ public class Parser {
 		}
 	}
 
+	/**
+	 * Checks if current token is of given type.
+	 *
+	 * @param type
+	 *            The type to be matched against.
+	 * @return True, if it is token of wanted type, false otherwise.
+	 */
 	private boolean isTokenOfType(TokenType type) {
 		return getCurrentTokenType() == type;
 	}
 
+	/**
+	 * Method used for getting current token value.
+	 *
+	 * @return current token value
+	 */
+	private Object getCurrentTokenValue() {
+		return lexer.getCurrentToken().getTokenValue();
+	}
+
+	/**
+	 * Method used for getting current token type.
+	 *
+	 * @return current token type
+	 */
+	private TokenType getCurrentTokenType() {
+		return lexer.getCurrentToken().getTokenType();
+	}
+
+	/**
+	 * Getter method used for getting the root node.
+	 * 
+	 * @return root node of the expression
+	 */
+	public Node getExpression() {
+		return node;
+	}
+
+	/**
+	 * Initialization method which calls {@link #e1()}.
+	 * 
+	 * @throws ParserException
+	 *             if current token is unexpected
+	 */
 	private void parse() {
 
 		lexer.nextToken();
@@ -44,13 +103,19 @@ public class Parser {
 		if (!isTokenOfType(TokenType.EOF)) {
 
 			String message = String.format("Unexpected token: Type: %s, Value: %s, Value is instance of: %s",
-					getCurrentTokenType(), getCurrentTokenValue(),
-					getCurrentTokenValue().getClass().getName());
+					getCurrentTokenType(), getCurrentTokenValue(), getCurrentTokenValue().getClass().getName());
 
 			throw new ParserException(message);
 		}
 	}
 
+	/**
+	 * First method of our recursive descent. It creates
+	 * {@link BinaryOperatorNode} if there is one or more "or" in the expression
+	 * after current node value.
+	 *
+	 * @return the node
+	 */
 	private Node e1() {
 		Node node = e2();
 
@@ -62,8 +127,8 @@ public class Parser {
 		nodes.add(node);
 		while (getCurrentTokenValue().equals("or")) {
 			if (lexer.nextToken().getTokenType() == TokenType.EOF) {
-				String message = String.format("Unexpected token found: {Type: %s, Value: %s}.",
-						getCurrentTokenType(), getCurrentTokenValue());
+				String message = String.format("Unexpected token found: {Type: %s, Value: %s}.", getCurrentTokenType(),
+						getCurrentTokenValue());
 
 				throw new ParserException(message);
 			}
@@ -71,7 +136,7 @@ public class Parser {
 
 			BinaryOperatorNode orNode = new BinaryOperatorNode("or", nodes, (t, s) -> t || s);
 
-			if (endOfOperator("or")) {
+			if (isTokenOfType(TokenType.EOF) || !getCurrentTokenValue().equals("or")) {
 				return orNode;
 			}
 
@@ -80,6 +145,13 @@ public class Parser {
 
 	}
 
+	/**
+	 * Second method of our recursive descent. It creates
+	 * {@link BinaryOperatorNode} if there is one or more "xor" in the
+	 * expression after current node value.
+	 *
+	 * @return the node
+	 */
 	private Node e2() {
 		Node node = e3();
 
@@ -90,8 +162,8 @@ public class Parser {
 		nodes.add(node);
 		while (getCurrentTokenValue().equals("xor")) {
 			if (lexer.nextToken().getTokenType() == TokenType.EOF) {
-				String message = String.format("Unexpected token found: {Type: %s, Value: %s}.",
-						getCurrentTokenType(), getCurrentTokenValue());
+				String message = String.format("Unexpected token found: {Type: %s, Value: %s}.", getCurrentTokenType(),
+						getCurrentTokenValue());
 
 				throw new ParserException(message);
 			}
@@ -99,7 +171,7 @@ public class Parser {
 
 			BinaryOperatorNode xorNode = new BinaryOperatorNode("xor", nodes, (t, s) -> t ^ s);
 
-			if (endOfOperator("xor")) {
+			if (isTokenOfType(TokenType.EOF) || !getCurrentTokenValue().equals("xor")) {
 				return xorNode;
 			}
 		}
@@ -107,6 +179,13 @@ public class Parser {
 
 	}
 
+	/**
+	 * Third method of our recursive descent. It creates
+	 * {@link BinaryOperatorNode} if there is one or more "and" in the
+	 * expression after current node value.
+	 *
+	 * @return the node
+	 */
 	private Node e3() {
 		Node node = e4();
 
@@ -118,31 +197,38 @@ public class Parser {
 		nodes.add(node);
 		while (getCurrentTokenValue().equals("and")) {
 			if (lexer.nextToken().getTokenType() == TokenType.EOF) {
-				String message = String.format("Unexpected token found: {Type: %s, Value: %s}.",
-						getCurrentTokenType(), getCurrentTokenValue());
+				String message = String.format("Unexpected token found: {Type: %s, Value: %s}.", getCurrentTokenType(),
+						getCurrentTokenValue());
 
 				throw new ParserException(message);
 			}
 			nodes.add(e4());
 			BinaryOperatorNode andNode = new BinaryOperatorNode("and", nodes, (t, s) -> t && s);
 
-			if (endOfOperator("and")) {
+			if (isTokenOfType(TokenType.EOF) || !getCurrentTokenValue().equals("and")) {
 				return andNode;
 			}
 		}
 		return node;
 	}
 
+	/**
+	 * Fourth method of our recursive descent. It creates
+	 * {@link UnaryOperatorNode} if there is "not" followd by node value in the
+	 * expression.
+	 *
+	 * @return the node
+	 */
 	private Node e4() {
 
 		if (getCurrentTokenValue().equals("not")) {
 			if (lexer.nextToken().getTokenType() == TokenType.EOF) {
-				String message = String.format("Unexpected token found: {Type: %s, Value: %s}.",
-						getCurrentTokenType(), getCurrentTokenValue());
+				String message = String.format("Unexpected token found: {Type: %s, Value: %s}.", getCurrentTokenType(),
+						getCurrentTokenValue());
 
 				throw new ParserException(message);
 			}
-			
+
 			Node node = e4();
 			UnaryOperatorNode notNode = new UnaryOperatorNode("not", node, t -> !t);
 
@@ -153,73 +239,38 @@ public class Parser {
 
 	}
 
+	/**
+	 * Fifth method of our recursive descent. It creates {@link ConstantNode} or
+	 * {@link VariableNode} if current token matches expected one. If there is
+	 * opened bracket and closed bracket and expression between it starts
+	 * recursive descendig all over again just for that expression.
+	 *
+	 * @return the node
+	 */
 	private Node e5() {
 		Token currentToken = lexer.getCurrentToken();
 		Node node;
 
-		switch (currentToken.getTokenType()) {
-		case VARIABLE:
+		if (isTokenOfType(TokenType.VARIABLE)) {
 			node = new VariableNode((String) currentToken.getTokenValue());
-			break;
-
-		case CONSTANT:
+		} else if (isTokenOfType(TokenType.CONSTANT)) {
 			node = new ConstantNode((boolean) currentToken.getTokenValue());
-			break;
-
-		case OPEN_BRACKET:
+		} else if (isTokenOfType(TokenType.OPEN_BRACKET)) {
 			lexer.nextToken();
 			node = e1();
 
 			if (getCurrentTokenType() != TokenType.CLOSED_BRACKET) {
 				throw new ParserException("Expected ')' but found " + getCurrentTokenType() + ".");
 			}
-			break;
-
-		default:
-			String message = String.format("Unexpected token found: {Type: %s, Value: %s}.",
-					getCurrentTokenType(), getCurrentTokenValue());
+		} else {
+			String message = String.format("Unexpected token found: {Type: %s, Value: %s}.", getCurrentTokenType(),
+					getCurrentTokenValue());
 			throw new ParserException(message);
-
 		}
 
 		lexer.nextToken();
-
 		return node;
 
-	}
-
-	private Object getCurrentTokenValue() {
-		return lexer.getCurrentToken().getTokenValue();
-	}
-
-	private TokenType getCurrentTokenType() {
-		return lexer.getCurrentToken().getTokenType();
-	}
-
-	/**
-	 * Private method used in methods {@link Parser#e1()}, {@link Parser#e2()}
-	 * and {@link Parser#e3()} for determining if next token is same operator
-	 * and should we continue with our loop or return current found node.
-	 * 
-	 * @param operatorValue
-	 *            current operator value, can be only one of those: "or", "and",
-	 *            "xor"
-	 * @return <code>true</code> if next token is of type EOF or different value
-	 *         than operatorValue given as argument of this method,
-	 *         <code>false</code> otherwise
-	 */
-	private boolean endOfOperator(String operatorValue) {
-		return getCurrentTokenType() == TokenType.EOF
-				|| !getCurrentTokenValue().equals(operatorValue);
-	}
-
-	/**
-	 * Getter method used for getting the base node of expression.
-	 * 
-	 * @return base node of the expression
-	 */
-	public Node getExpression() {
-		return node;
 	}
 
 }
